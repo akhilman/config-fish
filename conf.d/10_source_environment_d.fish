68db0fee7263fe4c32e6cde0d2146b798fd37479
Ildar Akhmetgaleev <akhilman@gmail.com>
2024-09-11 12:36:52 +0700

    Replace ENVIRONMENT_LOADED with ENVIRONMENT_D_LOADED

--- conf.d/10_source_environment_d.fish
+++ conf.d/10_source_environment_d.fish
@@ -1,5 +1,5 @@
 if status is-login
-    and not set -q ENVIRONMENT_LOADED
+    and not set -q ENVIRONMENT_D_LOADED
     and command -q envsubst
     for f in $HOME/.config/environment.d/*.conf
         cat $f | string match -qr '^(?<key>\w+?)=(?<value>[^\#]+).*$'
